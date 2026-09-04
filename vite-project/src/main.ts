e97360c3a46300683636c5874d6f08b4c1bb229c
yesuf-star <ibnuesmael2022@gmail.com>
2026-09-04 15:58:49 +0300

    initial commit

---
+++ vite-project/src/main.ts
@@ -0,0 +1,20 @@
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = `
+  <h1>My TypeScript App</h1>
+
+  <input id="nameInput" placeholder="Enter your name">
+
+  <button id="helloButton">Say Hello</button>
+
+  <p id="message"></p>
+`;
+
+const input = document.querySelector<HTMLInputElement>('#nameInput')!;
+const button = document.querySelector<HTMLButtonElement>('#helloButton')!;
+const message = document.querySelector<HTMLParagraphElement>('#message')!;
+
+button.addEventListener('click', () => {
+  const name: string = input.value;
+  message.textContent = `Hello, ${name}! 👋`;
+});
